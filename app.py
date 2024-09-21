6d7bc7a121edeaa8c4445f5eb081528d9626f059
Dean Kinyua <kinyuadean@gmail.com>
2024-09-21 14:48:27 +0300

    constructors allow you to inject attributes into objects as you initialize them

--- app.py
+++ app.py
@@ -15,17 +15,17 @@ finding out the attributes of the object. If you execute the following twolines 
 
 """
 
-print(multilinestring)
+# print(multilinestring)
 
-n = 5
-print(dir(n))
+# n = 5
+# print(dir(n))
 string1 = "boyboyss1"
 
-print(string1[2])
+# print(string1[2])
 
 course = 'Python for beginners'
 
-print(course[0: 5])
+# print(course[0: 5])
 
 # we can use formatted strings to ours advantage
 
@@ -38,11 +38,11 @@ print(course[0: 5])
 # contains = 'Python' in course
 # print(contains)
 
-print(7 % 5)
+# print(7 % 5)
 
 variable = 10
 variable += 10
-print(variable)
+# print(variable)
 
 is_hot = True
 is_cold = False
@@ -90,11 +90,61 @@ customer = {
     "is_verified": True
 
 }
-print(customer['age'])
+
+print("The customer is " + str(customer['age']) + " years of age")
 
 
-def greet_user(name):
-    print(f"Hi {name}")
+def greet_user(first_name, last_name):
+    print(f"Hi {first_name} {last_name}")
 
 
-print(greet_user("John"))
+print(greet_user("John", "Smith"))
+# try:
+#     age = int(input("Age: "))
+#     income = 20000
+#     risk = income / age
+#     print(age)
+# except ValueError:
+#     print("Not a valid number")
+# except ZeroDivisionError:
+#     print("Age cannot be 0")
+
+# try:
+#     age = int(input("Age: "))
+#     income = 2000
+#     print(age)
+# except ValueError:
+#     print("Not a valid number")
+# except ZeroDivisionError:
+#     print("Age cannot be Zero")
+
+# print("Age cannot be Zero")
+# print("I am a Beast")
+#
+# print("Siezi toa ganji dadi")
+# print("Siezi toa ganji dadi")
+print("I do not trust the Adani deal and I also unfortunately don't have a say in the matter")
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print(f'move to point ({self.x}, {self.y})')
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def talk(self):
+        print(f"{self.name} can talk incredibly fluently")
+
+
+point2 = Point(0, 0)
+
+person3 = Person('Dean Kinyua')
+
+person3.talk()
